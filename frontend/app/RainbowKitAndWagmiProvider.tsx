0aa78377d4c12959887f16d092509ea7b896d456
Tony-S201 <swierz.tony@gmail.com>
2024-07-17 22:01:01 +0200

    build: re-add all files

---
+++ frontend/app/RainbowKitAndWagmiProvider.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+// RainbowKit + Wagmi + React TanStack Query
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    hardhat
+  } from 'wagmi/chains';
+
+interface RainbowKitProviderProps {
+    children: React.ReactNode;
+}
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string,
+  chains: [mainnet, polygon, optimism, arbitrum, base, hardhat],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+const RainbowKitAndWagmiProvider: React.FC<RainbowKitProviderProps> = ({children}: any):React.JSX.Element => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default RainbowKitAndWagmiProvider
