c752e4dbcda9cdf712e9140215c5a19602070ad7
Tony-S201 <swierz.tony@gmail.com>
2024-09-21 18:03:29 +0200

    front: my collection page template

--- frontend/app/my-collection/page.tsx
+++ frontend/app/my-collection/page.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+
+'use client';
+
+import { useState } from "react";
+
+// UI components
+import { Button, TextField } from "@mui/material";
+import { Card, CardActions, CardContent, CardMedia } from "@mui/material";
 
 const MyCollectionPage: React.FunctionComponent = (): JSX.Element => {
+
+  interface nftItem {
+    name: string,
+    address: string,
+    attributes: {}
+  }
+
+  const [nftItems, setNftItems] = useState<nftItem[]>([]);
+
   return (
-    <div>My Collection</div>
+    <div className="min-h-screen p-20">
+      <div className="flex items-center justify-center gap-8">
+        <TextField placeholder="Search..."></TextField>
+        <Button variant="contained">Filter</Button>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image="image.jpg"
+            title="happy apple"
+          />
+          <CardContent>
+              <h2 className="text-2xl">Happy Apple</h2>
+              <p>Here an happy apple</p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Transfer</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </div>
   )
 }
 
