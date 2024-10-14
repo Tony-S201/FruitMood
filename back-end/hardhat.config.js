e32a3ee804367539984973ef11f1c6be3568ea53
Tony-S201 <swierz.tony@gmail.com>
2024-10-14 14:16:08 +0200

    core: backend: edit config

--- back-end/hardhat.config.js
+++ back-end/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY = process.env.NETWORK_API_KEY;
+const PK = process.env.NETWORK_PK;
+
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    arbitrum_sepolia: {
+      url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PK]
+    },
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PK]
+    }
+  },
 };
